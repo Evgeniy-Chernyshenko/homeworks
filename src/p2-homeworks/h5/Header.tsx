2953336f766122440aff570d1482bcd0f6fb6628
Evgeniy-Chernyshenko <yevgeniyche@gmail.com>
2022-02-07 18:22:21 +0300

    5 homework completed

--- src/p2-homeworks/h5/Header.tsx
+++ src/p2-homeworks/h5/Header.tsx
@@ -1,12 +1,53 @@
-import React from 'react'
+import React, { useState } from "react";
+import { NavLink, Link } from "react-router-dom";
+import { PATH } from "./Routes";
+import styles from "./Header.module.css";
 
 function Header() {
-    return (
-        <div>
-            // add NavLinks
+  const [isShow, setIsShow] = useState<boolean>(false);
 
+  const setIsActive = (props: { isActive: boolean }) =>
+    props.isActive ? styles.active : "";
+
+  return (
+    <div className={styles.header}>
+      <div className={styles.panel}>
+        <div>
+          <Link to={PATH.PRE_JUNIOR} className={styles.logo}>
+            Ignat's homeworks
+          </Link>
         </div>
-    )
+        <button
+          onClick={() => {
+            setIsShow(!isShow);
+          }}
+          className={styles.hamburger}
+        >
+          {isShow ? "X" : "☰"}
+        </button>
+      </div>
+      <ul
+        onClick={() => setIsShow(false)}
+        className={`${styles.menu} ${isShow ? styles.show : ""}`}
+      >
+        <li>
+          <NavLink className={setIsActive} to={PATH.PRE_JUNIOR}>
+            pre-junior
+          </NavLink>
+        </li>
+        <li>
+          <NavLink className={setIsActive} to={PATH.JUNIOR}>
+            junior
+          </NavLink>
+        </li>
+        <li>
+          <NavLink className={setIsActive} to={PATH.JUNIOR_PLUS}>
+            junior+
+          </NavLink>
+        </li>
+      </ul>
+    </div>
+  );
 }
 
-export default Header
+export default Header;
--- src/p2-homeworks/h5/pages/Error404.tsx
+++ src/p2-homeworks/h5/pages/Error404.tsx
@@ -1,13 +1,16 @@
-import React from 'react'
+import React from "react";
+import styles from "./Error404.module.css";
 
 function Error404() {
-    return (
-        <div>
-            <div>404</div>
-            <div>Page not found!</div>
-            <div>—ฅ/ᐠ.̫ .ᐟ\ฅ—</div>
-        </div>
-    )
+  return (
+    <div className={styles.errorPage}>
+      <div>
+        <div>404</div>
+        <div>Page not found!</div>
+        <div>—ฅ/ᐠ.̫ .ᐟ\ฅ—</div>
+      </div>
+    </div>
+  );
 }
 
-export default Error404
+export default Error404;
